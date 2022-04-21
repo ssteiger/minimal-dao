e368ca20af0d79236a167d171fa6b899b021a48e
Sören Steiger <soren1@gmx.net>
2022-04-21 14:22:31 +0200

    Add mint function to MyToken

--- packages/hardhat/contracts/MyToken.sol
+++ packages/hardhat/contracts/MyToken.sol
@@ -6,4 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor() ERC20("MyToken", "MTK") {}
+
+    // TODO: anyone can mint
+    //       add access control: https://wizard.openzeppelin.com
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
 }
