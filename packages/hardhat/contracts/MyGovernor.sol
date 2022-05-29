64c44fe21bc3a0be305800c630366f3d2ba16097
Sören Steiger <soren1@gmx.net>
2022-05-29 12:56:32 +0200

    Set proposal threshold to 1 nft

--- packages/hardhat/contracts/MyGovernor.sol
+++ packages/hardhat/contracts/MyGovernor.sol
@@ -11,7 +11,7 @@ import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFractio
 contract MyGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
     constructor(IVotes _token)
         Governor("MyGovernor")
-        GovernorSettings(1 /* 1 block */, 45818 /* 1 week */, 1e18)
+        GovernorSettings(1 /* 1 block */, 45818 /* 1 week */, 1)
         GovernorVotes(_token)
         GovernorVotesQuorumFraction(50)
     {}
