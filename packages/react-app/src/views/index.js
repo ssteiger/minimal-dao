a37c04bdc9a1a587064af3ab30686b930139d9c5
Sören Steiger <soren1@gmx.net>
2022-05-29 06:08:30 +0200

    Fix exports

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -1,4 +1,7 @@
 export { default as Home } from './Home';
+export { default as ProposalCreator } from './ProposalCreator';
+export { default as ProposalsOverview } from './ProposalsOverview/';
+export { default as VoteOnProposals } from './VoteOnProposals/';
 export { default as ExampleUI } from './ExampleUI';
 export { default as Hints } from './Hints';
 export { default as Subgraph } from './Subgraph';
